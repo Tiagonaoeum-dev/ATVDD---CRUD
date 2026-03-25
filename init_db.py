bb0e0b305bbf3a14663ba76e55eea9e8a11e56e9
Tigas <0001158193@senaimgaluno.com.br>
2026-03-24 21:18:38 -0300

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,23 +1,22 @@
 import sqlite3
 
 def criar_banco():
-    conn = sqlite3.connect('database.db')
+    conn = sqlite3.connect('loja.db')
     cursor = conn.cursor()
 
     cursor.execute("""
-    CREATE TABLE IF NOT EXISTS livros (
+    CREATE TABLE IF NOT EXISTS jogos (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         titulo TEXT NOT NULL,
-        autor TEXT NOT NULL,
-        ano INTEGER,
-        disponivel BOOLEAN DEFAULT 1
+        genero TEXT NOT NULL,
+        preco REAL NOT NULL,
+        estoque INTEGER NOT NULL
     )
     """)
 
     conn.commit()
     conn.close()
-
-    print("Banco de dados criado com sucesso!")
+    print("Banco criado com sucesso!")
 
 if __name__ == "__main__":
     criar_banco()
